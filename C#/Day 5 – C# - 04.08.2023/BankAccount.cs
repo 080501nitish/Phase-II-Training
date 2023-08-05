fe7c63523f0c6192f00dd0e27c77eab312a32e3f
080501nitish <141027304+080501nitish@users.noreply.github.com>
2023-08-05 15:19:10 +0530

    Add files via upload

---
+++ C#/Day 5 – C# - 04.08.2023/BankAccount.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal class BankAccount
+    {
+        private readonly int AccountNumber;
+        private string AccountHolderName;
+        public static int Balance=0;
+        public BankAccount(int AccountNumber,string AccountHolderName)
+        {
+            this.AccountNumber = AccountNumber;
+            AccountHolderName1 = AccountHolderName;
+        }
+        public string AccountHolderName1 { get => AccountHolderName; set => AccountHolderName = value; }
+
+        public void deposit()
+        {
+            Console.WriteLine("Enter The Amount to Deposit");
+            int Dep_Amount = Convert.ToInt32(Console.ReadLine());
+            Balance = Balance + Dep_Amount;
+        }
+
+        public void withdraw()
+        {
+            Console.WriteLine("Enter The Amount to Withdraw");
+            int Wit_Amount = Convert.ToInt32(Console.ReadLine());
+            if (Wit_Amount <= Balance)
+            {
+                Balance = Balance - Wit_Amount;
+            }
+            else
+            {
+                Console.WriteLine("\n Insufficient Balance \n");
+            }
+            
+        }
+
+        public void PrintBalance()
+        {
+            Console.WriteLine($"\n Account Number : {AccountNumber} \n Account Holder Name : {AccountHolderName1} \n Balance : {Balance} \n");
+        }
+    }
+}
---
+++ C#/Day 5 – C# - 04.08.2023/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    internal class Book
+    {
+        private readonly int bookId;
+        private string Title;
+        private string Author;
+        private bool IsAvailable;
+
+        public Book(int bookId, string title, string author,bool IsAvailable)
+        {
+            this.bookId = bookId;
+            Title1 = title;
+            Author1 = author;
+            IsAvailable1 = IsAvailable;
+        }
+        public string Title1 { get => Title; set => Title = value; }
+        public string Author1 { get => Author; set => Author = value; }
+        public bool IsAvailable1 { get => IsAvailable; set => IsAvailable = value; }
+
+        
+        
+    }
+}
