eae58bcc82ef36aeb0c4dff5e83b9321e62915f6
080501nitish <141027304+080501nitish@users.noreply.github.com>
2023-08-06 22:34:34 +0530

    Add files via upload

---
+++ C#/Day 5 – C# - 04.08.2023/Assignment 1/Program.cs
@@ -0,0 +1,31 @@
+﻿using Bank;
+using System.Xml.Serialization;
+
+Console.WriteLine("Enter your Account Number");
+int AccountNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter your Account Holder Name");
+string? AccountHolderName = Console.ReadLine();
+BankAccount Account = new BankAccount(AccountNumber,AccountHolderName);
+int choice;
+do
+{
+    Console.WriteLine("\nEnter your Choice \n 1.Deposit \n 2.Withdraw \n 3.CheckBalance \n 4.End\n");
+    choice = Convert.ToInt32(Console.ReadLine());
+    switch (choice)
+    {
+        case 1:
+            Account.deposit();
+            break;
+        case 2:
+            Account.withdraw();
+            break;
+        case 3:
+            Account.PrintBalance(); 
+            break;
+        case 4:
+            break;
+        default:
+            Console.WriteLine("\nYour choice is wrong\nPlease try again...\n");
+            break;
+    }
+}while(choice!=4);
