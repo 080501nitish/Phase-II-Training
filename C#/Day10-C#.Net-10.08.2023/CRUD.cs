5909b5363b4848da731d2a7787ecf2c5ec0f0404
080501nitish <141027304+080501nitish@users.noreply.github.com>
2023-08-11 06:28:31 +0530

    Add files via upload

---
+++ C#/Day10-C#.Net-10.08.2023/CRUD.cs
@@ -0,0 +1,46 @@
+﻿using EFDBF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDBF
+{
+    internal class CRUD
+    {
+        public void AddNewDept()
+        {
+            using (var context = new EmployeeMgmtContext())
+            {
+                var dept = new Dept()
+                {
+                    Deptno = 10,
+                    Dname="HR"
+                    
+                };
+                context.Depts.Add(dept);
+                context.SaveChanges();
+            }
+        }
+
+        public void UpdateDept() 
+        {
+            var context = new EmployeeMgmtContext();
+            var seldept=context.Depts.FirstOrDefault(d => d.Deptno == 10);
+            seldept.Dname = "DEV";
+            context.SaveChanges();
+
+        }
+
+        public void DeleteDept()
+        {
+            var context = new EmployeeMgmtContext();
+            var seldept = context.Depts.FirstOrDefault(d => d.Deptno == 10);
+            context.Depts.Remove(seldept);
+            context.SaveChanges();
+        }
+
+        
+    }
+}
